35ba001fe23df849e2bb09658ce896d24f3ce72d
Zerobase42 <malbolgeisfun@naver.com>
2026-05-30 17:29:01 +0900

    read database.txt

--- cpp_minifier/script.js
+++ cpp_minifier/script.js
@@ -1,14 +1,22 @@
 console.log("script loaded");
-
-const db = [
-    '+', '-', '*', '/', '%', '?', ':', ';',
-    '(', ')', '{', '}', '[', ']',
-    ',', '==', '+=', '-=', '*=', '/=', '%=',
-    '&=', '|=', '^=', '>=', '<=', '=',
-    '>', '<', '!=', '&', '|', '&&', '||', '~'
-];
+let db = [];
 
 const HARD_TEXT = "궭뷁쉛뤒";
+
+async function loadDatabase() {
+
+    const text = await fetch("database.txt")
+        .then(r => r.text());
+
+    db = text
+        .split(/\r?\n/)
+        .map(x => x.trim())
+        .filter(Boolean);
+
+    console.log("database loaded:", db.length);
+}
+
+loadDatabase();
 
 function minify(code) {
 
@@ -20,39 +28,55 @@ function minify(code) {
         const stripped = line.trim();
 
         // 전처리문 유지
+        /*
         if (stripped.startsWith('#')) {
             result.push(stripped);
             continue;
         }
+        */
 
-        line = line.replaceAll('    ', HARD_TEXT);
+        line = line.replaceAll("    ", HARD_TEXT);
 
-        for (const a of db) {
-            line = line.replaceAll(' ' + a + ' ', a);
-            line = line.replaceAll(a + ' ', a);
-            line = line.replaceAll(' ' + a, a);
+        for (const op of db) {
+            line = line.replaceAll(" " + op + " ",op);
+            line = line.replaceAll(op + " ",op);
+            line = line.replaceAll(" " + op,op);
         }
 
         result.push(
-            line.replaceAll(HARD_TEXT, '    ')
+            line.replaceAll(HARD_TEXT,"    ")
         );
     }
 
-    code = result.join('\n');
+    code = result.join("\n");
 
     // // 주석 제거
-    code = code.replace(/\/\/[^\n]*/g, '');
+    code = code.replace(
+        /\/\/[^\n]*/g,
+        ""
+    );
 
     // /* */ 주석 제거
-    code = code.replace(/\/\*[\s\S]*?\*\//g, '');
+    code = code.replace(
+        /\/\*[\s\S]*?\*\//g,
+        ""
+    );
 
     // 빈 줄 제거
-    code = code.replace(/\n\s*\n+/g, '\n');
+    code = code.replace(
+        /\n\s*\n+/g,
+        "\n"
+    );
 
     return code;
 }
 
 function minifyCode() {
+
+    if (db.length === 0) {
+        alert("database.txt loading...");
+        return;
+    }
 
     const code =
         document.getElementById("input").value;
