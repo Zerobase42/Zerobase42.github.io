3bfff8e4e4647ef5a1150b31233d5f7de0e53105
Zerobase42 <malbolgeisfun@naver.com>
2026-05-30 17:08:02 +0900

    python to js

--- cpp_minifier/script.js
+++ cpp_minifier/script.js
@@ -1,13 +1,64 @@
-const code = document.getElementById("input").value;
+const db = [
+    '+', '-', '*', '/', '%', '?', ':', ';',
+    '(', ')', '{', '}', '[', ']',
+    ',', '==', '+=', '-=', '*=', '/=', '%=',
+    '&=', '|=', '^=', '>=', '<=', '=',
+    '>', '<', '!=', '&', '|', '&&', '||', '~'
+];
 
-pyodide.globals.set("cpp_code", code);
+const HARD_TEXT = "궭뷁쉛뤒";
 
-await pyodide.runPythonAsync(`
-from minifier import minify
+function minify(code) {
 
-result = minify(cpp_code)
-`);
+    const lines = code.split('\n');
+    const result = [];
 
-const result = pyodide.globals.get("result");
+    for (let line of lines) {
 
-document.getElementById("output").textContent = result;
+        const stripped = line.trim();
+
+        // 전처리문 유지
+        if (stripped.startsWith('#')) {
+            result.push(stripped);
+            continue;
+        }
+
+        line = line.replaceAll('    ', HARD_TEXT);
+
+        for (const a of db) {
+            line = line.replaceAll(' ' + a + ' ', a);
+            line = line.replaceAll(a + ' ', a);
+            line = line.replaceAll(' ' + a, a);
+        }
+
+        result.push(
+            line.replaceAll(HARD_TEXT, '    ')
+        );
+    }
+
+    code = result.join('\n');
+
+    // // 주석 제거
+    code = code.replace(/\/\/[^\n]*/g, '');
+
+    // /* */ 주석 제거
+    code = code.replace(/\/\*[\s\S]*?\*\//g, '');
+
+    // 빈 줄 제거
+    code = code.replace(/\n\s*\n+/g, '\n');
+
+    return code;
+}
+
+function runPython() {
+
+    const code =
+        document.getElementById("input").value;
+
+    const result =
+        minify(code);
+
+    document
+        .getElementById("output")
+        .textContent = result;
+}
