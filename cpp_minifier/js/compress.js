bf78683b76e281073ab549b06ff7eba08b05c7c1
Zerobase42 <malbolgeisfun@naver.com>
2026-06-09 20:59:09 +0900

    change webpage

--- cpp_minifier/js/compress.js
+++ cpp_minifier/js/compress.js
@@ -3,7 +3,7 @@ console.log("compress.js loaded");
 import base85 from "https://cdn.jsdelivr.net/npm/@alttiri/base85@1.8.0/base85.min.js";
 
 export function compress(text){
-    const compressed=pako.gzip(text);
+    const compressed=pako.gzip(text,{level:9});
     return encodeURIComponent(base85.encode(compressed));
 }
 export function decompress(encoded){
